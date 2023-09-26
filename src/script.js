cd1184fb3a38e0d627a68bfd659caa327782601a
Maria cielo <sanchezmaria306@gmail.com>
2023-09-26 11:10:57 -0300

    aguegue bootstrap al proyecto

--- src/script.js
+++ src/script.js
@@ -8,24 +8,60 @@ const products = JSON.parse(window.localStorage.products);
 console.log(products);
 
 const ul = document.querySelector('#product-list');
+let html = '';
 
 if(ul){
     products.forEach(function(product,index) {
-        let li=fragment.appendChild(document.createElement("li"));
-        li.textContent=product.codigo;
-        let icono = document.createElement("img");
-        icono.src="img/boton-mas.png"
-        let btn = fragment.appendChild(document.createElement("a"));
-        btn.addEventListener("click",function(event){ saveId(index); }, false);
-        btn.setAttribute('href', 'product.html');
-        btn.classList.add("miBoton");
-        let br = fragment.appendChild(document.createElement("br"));
-        ul.appendChild(li);
-        ul.appendChild(btn);
-        ul.appendChild(br);
-        btn.appendChild(icono);
+        html += '<div class="col-xs-12 col-md-6 bootstrap snippets bootdeys">' + 
+                    '<div class="product-content product-wrap clearfix">' +
+                        '<div class="row">' + 
+                            '<div class="col-md-5 col-sm-12 col-xs-12">' +
+                                '<div class="product-image">' + 
+                                    '<img src="'+ product.imagen +'" alt="194x228" height="228" class="img-responsive">' +
+                                    '<span class="tag2 hot">' +
+                                        'HOT' +
+                                    '</span>' +
+                                '</div>' +
+                            '</div>' +
+                            '<div class="col-md-7 col-sm-12 col-xs-12">' +
+                                '<div class="product-deatil">' +
+                                    '<h5 class="name">' +
+                                        '<a href="#">' +
+                                            product.codigo +
+                                        '</a>' +
+                                    '</h5>' +
+                                    '<p class="price-container">' +
+                                        '<span>$'+ product.precio +'</span>' +
+                                    '</p>' +
+                                    '<span class="tag1"></span>' +
+                                '</div>' +
+                                '<div class="description">' +
+                                    '<p>'+ product.descripcion +'</p>' +
+                                '</div>' +
+                                '<div class="product-info smart-form">' +
+                                    '<div class="row">' +
+                                        '<div class="col-md-6 col-sm-6 col-xs-6">' +
+                                            '<a href="product.html" class="btn btn-success" onclick="saveId('+ index +')">Ver Detalle</a>' +
+                                        '</div>' +
+                                        '<div class="col-md-6 col-sm-6 col-xs-6">' +
+                                            '<div class="rating">' +
+                                                '<label for="stars-rating-5"><i class="fa fa-star"></i></label>' +
+                                                '<label for="stars-rating-4"><i class="fa fa-star"></i></label>' +
+                                                '<label for="stars-rating-3"><i class="fa fa-star text-primary"></i></label>' +
+                                                '<label for="stars-rating-2"><i class="fa fa-star text-primary"></i></label>' +
+                                                '<label for="stars-rating-1"><i class="fa fa-star text-primary"></i></label>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>';
 
     });
+
+    document.getElementById('section').innerHTML = html;
 }
 
 function saveId(id){
@@ -43,4 +79,11 @@ if(h1){
     h1.textContent = product.codigo;
     description.textContent = product.descripcion;
     img.src = product.imagen;
+    let lista = document.getElementById('puntos_claves');
+    let listaHtml = '';
+    product.claves.forEach(clave => {
+        listaHtml += '<li><i class="fa fa-check text-success"></i>'+ clave +'</li>';
+    });
+    console.log(listaHtml);
+    lista.innerHTML = listaHtml;
 }
