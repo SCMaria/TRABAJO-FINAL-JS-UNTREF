34da2e228d5b43a1e07dbe7019d3d26cbc5a34fc
Maria cielo <sanchezmaria306@gmail.com>
2023-09-29 21:34:10 -0300

    modificaciones

--- src/product.js
+++ src/product.js
@@ -1,39 +1,29 @@
-
-
-const productList = document.querySelector("#product-list");
 let products = [];
-const lista = async () => {
-    productList.innerHTML = "";
-    products = localStorage.getItem("products");
+products = localStorage.getItem("products");
+products = JSON.parse(products);
 
-
-    if (products == null) {
-        const response = await fetch("json/products.json");
-        product = await response.json();
-
-        localStorage.setItem("products", JSON.stringify(product));
-  }
-  if (typeof products == "string") {
-    products = JSON.parse(products);
-  }
-
-};
 
 
 const h1 = document.querySelector('#title');
 const description = document.querySelector('#description');
 const product = products[window.localStorage.productClick];
 const img = document.querySelector('#img');
+const precio= document.querySelector('#precio');
+const detalle = document.getElementById('detalle');
 
-if(h1){
+if (h1) {
     h1.textContent = product.codigo;
     description.textContent = product.descripcion;
     img.src = product.imagen;
-    let lista = document.getElementById('puntos_claves');
-    let listaHtml = '';
-    product.claves.forEach(clave => {
-        listaHtml += '<li><i class="fa fa-check text-success"></i>'+ clave +'</li>';
+    detalle.textContent = product.detalle;
+    precio.textContent += "$" + product.precio + "";
+    let lista = document.getElementById("puntos_claves");
+    let listaHtml = "";
+    product.claves.forEach((clave) => {
+      listaHtml +=
+        '<li><i class="fa fa-check text-success"></i>' + clave + "</li>";
     });
     console.log(listaHtml);
     lista.innerHTML = listaHtml;
-}
+  }
+  
